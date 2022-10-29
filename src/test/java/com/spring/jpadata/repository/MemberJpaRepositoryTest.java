8953bdcedc100eaaf63843d0256a359f833ccf34
KimGunWoo9595 <medneter@naver.com>
2022-10-29 10:39:41 +0900

    디비 연동 테스트

---
+++ src/test/java/com/spring/jpadata/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.spring.jpadata.repository;
+
+import com.spring.jpadata.entity.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("김건우");
+        Member save = memberJpaRepository.save(member);
+        Member ids = memberJpaRepository.find(save.getId());
+
+        assertThat(save.getId()).isEqualTo(ids.getId());
+    }
+
+
+
+}
