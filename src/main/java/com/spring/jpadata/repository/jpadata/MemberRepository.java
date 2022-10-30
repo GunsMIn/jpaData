730bfd1cced60fc7b238b8f314d6aee1b01292c8
KimGunWoo9595 <medneter@naver.com>
2022-10-30 23:37:33 +0900

    쿼리 메소드 네이밍

--- src/main/java/com/spring/jpadata/repository/jpadata/MemberRepository.java
+++ src/main/java/com/spring/jpadata/repository/jpadata/MemberRepository.java
@@ -3,6 +3,12 @@ package com.spring.jpadata.repository.jpadata;
 import com.spring.jpadata.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
                                             //<타입,pk매팽타입>
+
+    //쿼리 메소드드
+   List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+
 }
