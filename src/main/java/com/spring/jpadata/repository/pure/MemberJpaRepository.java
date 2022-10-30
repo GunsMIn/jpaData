cb53f9317ac437ad44fb7a00736269714beb1c90
KimGunWoo9595 <medneter@naver.com>
2022-10-30 16:23:09 +0900

    순수 JPA repository

---
+++ src/main/java/com/spring/jpadata/repository/pure/MemberJpaRepository.java
@@ -0,0 +1,53 @@
+package com.spring.jpadata.repository.pure;
+
+import com.spring.jpadata.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+//순수 jpa 레퍼지토리
+@Repository
+public class MemberJpaRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    //insert
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    //delete
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    //select by id (Optional)
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+
+    //select all
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    //count
+    public Long getCount() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+
+}
