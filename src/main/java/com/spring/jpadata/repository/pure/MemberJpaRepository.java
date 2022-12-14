0c93b483322f05ad51de5df0796cbf8b735acd21
KimGunWoo9595 <medneter@naver.com>
2022-12-14 16:35:42 +0900

    검색 queryDsl

--- src/main/java/com/spring/jpadata/repository/pure/MemberJpaRepository.java
+++ src/main/java/com/spring/jpadata/repository/pure/MemberJpaRepository.java
@@ -1,10 +1,17 @@
 package com.spring.jpadata.repository.pure;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpadata.dto.MemberSearchCondition;
+import com.spring.jpadata.dto.MemberTeamDto;
+import com.spring.jpadata.dto.QMemberResponse;
+import com.spring.jpadata.dto.QMemberTeamDto;
 import com.spring.jpadata.entity.Member;
 import com.spring.jpadata.entity.QMember;
+import com.spring.jpadata.entity.QTeam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,15 +20,18 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.spring.jpadata.entity.QMember.*;
+import static com.spring.jpadata.entity.QTeam.team;
 
 //순수 jpa 레퍼지토리
 @Repository
 @RequiredArgsConstructor
 public class MemberJpaRepository {
-    @PersistenceContext
+
     private EntityManager em;
 
-    private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory; // 메인application에 bean 으로 등록
+
+
 
     //insert
     public Member save(Member member) {
@@ -70,6 +80,44 @@ public class MemberJpaRepository {
     }
 
 
+    /**
+     * QueryDsl
+     **/
+    //검색하기
+    public List<MemberTeamDto> searchByBulider(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+        //null이 들어올 수 있고 ""이 들어올 수도 있다.
+        if (StringUtils.hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (StringUtils.hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        queryFactory
+                .select(
+                        new QMemberTeamDto(
+                                member.id.as("memberId"),
+                                member.username,
+                                member.age,
+                                team.id.as("teamId"),
+                                team.name.as("teamName")))
+                .from(member)
+                .where(builder)
+                .leftJoin(member.team, team)
+                .fetch();
+
+    }
+
+
+
+
     //count
     public Long getCount() {
         return em.createQuery("select count(m) from Member m", Long.class)
@@ -108,5 +156,7 @@ public class MemberJpaRepository {
         //단순 count라서 sort필요없다.
 
     }
+
+
 
 }
